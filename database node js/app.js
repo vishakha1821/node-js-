92e95e603cab5cfc4af2f87fb25f488c57b4e1a4
Vishakha <90548410+vishakha1821@users.noreply.github.com>
2022-05-14 20:48:40 +0530

    Add files via upload

--- database node js/app.js
+++ database node js/app.js
@@ -1,5 +1,20 @@
+const product = (a,b)=>{
+    return a*b;
+}
+
+console.log(product(3,5));
+
+const student = {
+    name : 'vishakha',
+    age: 21,
+
+    greet() {
+        console.log('hi, I am ' + this.name );
+    }
+
+    
+
+}
+student.greet();
 
 
-const fs = require('fs');
-
-fs.writeFileSync('hello.txt', 'hello from node js');
