278ace63d6942f0a2082059ee05a92b0f3a3497a
Vishakha <90548410+vishakha1821@users.noreply.github.com>
2022-05-17 14:03:02 +0530

    Add files via upload

--- database node js/server.js
+++ database node js/server.js
@@ -3,7 +3,27 @@ const http = require('http');
 
 
  const server = http.createServer((req, res) => {
-    console.log(req);
+    console.log( req.method, req.headers);
+
+    res.setHeader('Content-Type', 'text/html');
+    res.write('<html>');
+    res.write('<head>,y first Node JS page</head>');
+    res.write('<body><h1>Welcome to my first node JS Project</h1></body>');
+    res.write('</html>');
+    res.end();
+
+    switch(req.url) {
+        case "/home" :
+            res.write('<h2>Welcme home</h2>');
+            break;
+
+        case "/about_us" :
+            res.write('Welcome to about us');
+            break;
+
+    }
+
+   
 });
 
 server.listen(4000); 
